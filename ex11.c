b78125f7e2a25b61c86812edc512d78d5db9399a
shdowtail <34094780+shdowtail@users.noreply.github.com>
2022-10-06 19:46:23 +0300

    alo

---
+++ ex11.c
@@ -0,0 +1,18 @@
+/* what output does the following program fragment produce?*/
+/* initial answer: 1 2 4 16 32 64 128*/ //++
+#include <stdio.h>
+
+int main(void)
+{
+    int i = 1;
+    while (i <= 128)
+    {
+        printf("%d ", i);
+        i *= 2;
+    }
+    return 0;
+}
+
+//
+// Created by konket on 06/10/2022.
+//
