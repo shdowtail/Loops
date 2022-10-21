7949d49cc18754ed85d7fb695e7e2fa9805c4c63
konke <aleksandr.sinitson@outlook.com>
2022-10-21 18:26:09 +0300

    ++

--- Projects/loan-balance-2.c
+++ Projects/loan-balance-2.c
@@ -2,18 +2,19 @@
 // Created by konke on 20.10.22.
 //
 /* Program calculates the remaining balance after every month up to amount of months defined by the user input*/
-//broken
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
 bool flag;
-char *calculated_suffix;
+char *assigned_suffix;
 
 /*finds the appropriate suffix for a given month*/
- void num_suffix (long month_index_up)
+void num_suffix (long month_index_up)
 {
-  int i=0;
+
+  int i = 0;
   char num_str[3];
 
   sprintf (num_str, "%ld", month_index_up);
@@ -22,30 +23,27 @@ char *calculated_suffix;
 	{
 	  ++i;
 	}
-	--i;
-  if (num_str[i - 1] == '1' && num_str[i] == '1' || num_str[i - 1] == '1' && num_str[i] == '2'
-	  || num_str[i - 1] == '1' && num_str[i] == '3')
+  --i;
+  if (num_str[i - 1] == 49 && num_str[i] == 49
+	  || num_str[i - 1] == 49 && num_str[i] == 50
+	  || num_str[i - 1] == 49 && num_str[i] == 51
+	  || num_str[i] >= 52 && num_str[i] <= 57)
 	{
-	  calculated_suffix = "th";
-	  flag = true;
+	  assigned_suffix = "th";
 	}
-  else if (num_str[i] == '1')
+  else if (num_str[i] == 49)
 	{
-	  calculated_suffix = "st";
-	  flag = true;
-	}
-  else if (num_str[i] == '2')
-	{
-	  calculated_suffix = "nd";
-	  flag = true;
-	}
-  else if (num_str[i] == '3')
-	{
-	  calculated_suffix = "rd";
-	  flag = true;
+	  assigned_suffix = "st";
 	}
 
-
+  else if (num_str[i] == 50)
+	{
+	  assigned_suffix = "nd";
+	}
+  else if (num_str[i] == 51)
+	{
+	  assigned_suffix = "rd";
+	}
 }
 
 int main (void)
@@ -59,7 +57,6 @@ int main (void)
   char *ptr3;
   char str_requested_number_of_months[4];
   char *ptr4;
-
 
   long double amount;
   long double annual_interest_percent;
@@ -75,6 +72,7 @@ int main (void)
 /*User is prompted to enter needed values then the string input is converted to an according decimal format*/
   printf ("\nEnter the amount of loan: ");
   scanf ("%s", str_amount);
+
   amount = strtold (str_amount, &ptr1);
 
   printf ("Enter the interest rate: ");
@@ -94,34 +92,27 @@ int main (void)
   long double monthly_calculation;
   long double remaining_amount;
   long month_upCount_index = 1;
-
   /* The main calculation of remaining balance*/
+  monthly_interest_rate = annual_interest_percent / 100 / 12;
   while (nr_months_index > 0)
-
-{
-	  monthly_interest_rate = annual_interest_percent / 100 / 12;
+	{
 
 	  monthly_interest_payment = amount * monthly_interest_rate;
 
 	  monthly_non_interest_payment = monthly_payment - monthly_interest_payment;
 
-	  monthly_calculation = monthly_non_interest_payment + monthly_interest_rate;
+	  monthly_calculation = monthly_non_interest_payment + monthly_interest_payment;
 
 	  remaining_amount = amount - monthly_calculation;
 
 	  amount -= monthly_calculation;
 
 	  num_suffix (month_upCount_index);
-	  if(flag == true)
-	  {
-	  flag = false;
-	   printf ("\nBalance remaining after %ld%s payment : %.2Lf", month_upCount_index,calculated_suffix, remaining_amount);
 
-	  }
+	  printf ("\nBalance remaining after %ld%s payment : %.2Lf", month_upCount_index, assigned_suffix, remaining_amount);
 
-
-	  ++month_upCount_index;
-	  --nr_months_index;
+	  month_upCount_index++;
+	  nr_months_index--;
 	}
-  	return 0;
+  return 0;
 }
